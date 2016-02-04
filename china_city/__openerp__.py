efc592de797ff760b1062c94221b779b06738410
fanxiao.kong <fanxiao.kong@qunar.com>
2016-02-04 17:59:55 +0800

    中国行政区域划分

---
+++ china_city/__openerp__.py
@@ -0,0 +1,35 @@
+#-*- coding:utf-8 -*-
+##############################################################################
+#
+#    Powered By Rainsoft(QingDao) Author:Kevin Kong 2014 (kfx2007@163.com)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+		"name":u"china city",
+		"version":"1.0",
+		"description":u"""
+China City
+================================================================
+China City model is created for customers who are loacated in China main land.
+This model changes the format of partner's address,makes it looks much better from chinese's view.
+		  """,
+		"author":"Kevin Kong",
+		"website":"http://www.qdrainsoft.com",
+		"depends":["base"],
+                "data":["hm_city_view.xml","hm_region_view.xml","hm_partner_view.xml"],
+		"installable":True,
+		"category":"Generic Modules/Others"
+}
